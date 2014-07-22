492a18225fea0b5c7fda69664d469f86915618d5
Rita Cheng <rita@ritacheng.com>
2014-07-22 12:48:49 -0700

    feature: preloading is working

--- src/app/common/user.js
+++ src/app/common/user.js
@@ -1,1 +1,45 @@
-(function(){var t;t=angular.module("common.user_storage",[]),t.factory("gameStorage",function(){var t,e;return t={current_round:1,score:0},e={get:function(e){return sessionStorage.getItem(e)||t[e]},increment:function(t,r){return e.put(t,parseInt(e.get(t))+parseInt(r))},put:function(t,e){return sessionStorage.setItem(t,e)}}}),t.factory("persistentStorage",function(){var t,e;return t={high_score:0},e={get:function(e){return localStorage.getItem(e)||t[e]},increment:function(t,r){return e.put(t,parseInt(e.get(t))+parseInt(r))},put:function(t,e){return localStorage.setItem(t,e)}}})}).call(this);
+(function() {
+  var users;
+
+  users = angular.module("common.user_storage", []);
+
+  users.factory("gameStorage", function() {
+    var defaults, service;
+    defaults = {
+      "current_round": 1,
+      "score": 0
+    };
+    service = {
+      get: function(storageID) {
+        return sessionStorage.getItem(storageID) || defaults[storageID];
+      },
+      increment: function(storageID, n) {
+        return service.put(storageID, parseInt(service.get(storageID)) + parseInt(n));
+      },
+      put: function(storageID, game) {
+        return sessionStorage.setItem(storageID, game);
+      }
+    };
+    return service;
+  });
+
+  users.factory("persistentStorage", function() {
+    var defaults, service;
+    defaults = {
+      "high_score": 0
+    };
+    service = {
+      get: function(storageID) {
+        return localStorage.getItem(storageID) || defaults[storageID];
+      },
+      increment: function(storageID, n) {
+        return service.put(storageID, parseInt(service.get(storageID)) + parseInt(n));
+      },
+      put: function(storageID, info) {
+        return localStorage.setItem(storageID, info);
+      }
+    };
+    return service;
+  });
+
+}).call(this);
